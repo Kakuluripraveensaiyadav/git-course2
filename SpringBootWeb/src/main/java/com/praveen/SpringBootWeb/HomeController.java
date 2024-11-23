546eefef88c5873508052754acb6d8204fc06752
Praveen sai <praveensaiyadav126@gmail.com>
2024-11-24 00:13:29 +0530

    first commit

---
+++ SpringBootWeb/src/main/java/com/praveen/SpringBootWeb/HomeController.java
@@ -0,0 +1,20 @@
+package com.praveen.SpringBootWeb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.PrintWriter;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home()
+    {
+
+        System.out.println("home1 method");
+        return "Home.jsp";
+    }
+
+}
