f7dfacd95ae07771a46963a03e961af8f11848bc
Praveen sai <praveensaiyadav126@gmail.com>
2024-11-24 00:32:19 +0530

    first change

--- SpringBootWeb/src/main/java/com/praveen/SpringBootWeb/SpringBootWebApplication.java
+++ SpringBootWeb/src/main/java/com/praveen/SpringBootWeb/SpringBootWebApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootWebApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(SpringBootWebApplication.class, args);
 		Tomcat tomcat=new Tomcat();
 		tomcat.setPort(8088);
